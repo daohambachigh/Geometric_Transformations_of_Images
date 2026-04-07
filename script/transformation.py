4ac795dc2061bfdb6d2fe334bf4454251eb9e3cf
daohambachigh <thang.nguyenduc7624@gmail.com>
2026-04-07 07:48:27 +0700

    Coloured output image

--- script/transformation.py
+++ script/transformation.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 import cv2 as cv
-img = cv.imread('input_images/logo.jpg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('input_images/logo.jpg')
 assert img is not None, "file could not be read, check with os.path.exists()"
-rows,cols = img.shape
+rows,cols = img.shape[:2]
 M = np.float32([[1,0,100],[0,1,50]])
 dst = cv.warpAffine(img,M,(cols,rows))
 cv.imshow('img',dst)
